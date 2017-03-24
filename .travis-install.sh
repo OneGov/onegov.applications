5efbd87c1c7145caca4344faa39b4bc6388601aa
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-03-24 11:59:18 +0100

    Switch to the new elasticsearch testing regime

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,7 +1,2 @@
-#!/usr/bin/env sh
+#!/bin/bash
 python install_requirements.py
-
-if [ "$ES_URL" != 'no' ]; then
-    mkdir /tmp/elasticsearch
-    wget -O - "$ES_URL" | tar xz --directory=/tmp/elasticsearch --strip-components=1
-fi
