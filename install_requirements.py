59bf74ffd2fddaa0bda61f237922edefef55ee9b
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-08-03 14:16:37 +0200

    https://github.com/onegov/onegov.testing

--- install_requirements.py
+++ install_requirements.py
@@ -6,7 +6,7 @@ import os
 os.system('pip install --upgrade pip')
 os.system('pip install --upgrade setuptools')
 os.system('pip --version')
-os.system('pip install git+git://github.com/OneGov/onegov.testing.git#egg=onegov.testing')
+os.system('pip install git+git://github.com/OneGov/onegov_testing.git#egg=onegov_testing')
 
 with open('onegov/applications/applications.json') as f:
     applications = json.load(f)
