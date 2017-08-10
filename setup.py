4572eb746edf1cbeb51374a889ecbce43b3befd3
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-08-10 14:51:04 +0200

    Release 2017.8.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 name = 'onegov.applications'
 description = ('Meta-package containing all OneGov Cloud applications.')
-version = '2017.8.1'
+version = '2017.8.2'
 
 
 def get_long_description():
