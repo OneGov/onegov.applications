4b942017110aba56d72f2375b7ff0c6f501b5559
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-11-15 09:44:25 +0100

    Skip installation for untagged commits as well

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python install_requirements.py
+if [ "${TRAVIS_TAG}" == "" ]; then
+    echo "Skipping tests because this is not a tagged commit"
+else
+    python install_requirements.py
+fi
