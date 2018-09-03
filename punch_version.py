4f3ee99f982e8de9b8877ddcf6e23fce155f6cfc
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-09-03 10:56:35 +0200

    Release 2018.9.0

--- punch_version.py
+++ punch_version.py
@@ -1,3 +1,3 @@
 year = 2018
-month = 8
-build = 16
+month = 9
+build = 0
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 name = 'onegov.applications'
 description = ('Meta-package containing all OneGov Cloud applications.')
-version = '2018.8.16'
+version = '2018.9.0'
 
 
 def get_long_description():
