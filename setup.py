26878ebe635ba1a0f41ba6356120512a80bb2671
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2019-05-14 09:37:29 +0200

    Release 2019.5.15

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 name = 'onegov.applications'
 description = ('Meta-package containing all OneGov Cloud applications.')
-version = '2019.5.14'
+version = '2019.5.15'
 
 
 def get_long_description():
